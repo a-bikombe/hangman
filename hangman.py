f7058dd9a6d4424b738828344727a2747f38e45e
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2023-11-06 12:32:13 -0500

    guess

--- hangman.py
+++ hangman.py
@@ -66,4 +66,30 @@ Check
 
 """
 
-CHOSEN_WORD = input('Player 1, choose a word: ')
+chosen_word = input('Player 1, choose a word: ')
+blank_spaces = "_ " * len(chosen_word)
+
+print(
+"""
++------+
+|      |
+|	   
+|	  
+|     
+|
+==========
+""")
+print(blank_spaces)
+
+def guess(chosen_word):
+	letter_guess = input('Player 2, guess a letter: ')
+	if (letter_guess in chosen_word):
+		for x in chosen_word:
+			if (x == letter_guess):
+				print(x, end =" ")
+			else:
+				print("_", end =" ")
+	else:
+		print(0)
+
+guess(chosen_word)
